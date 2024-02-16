ad402039857c564ca0d24d80d8678686bbbba164
denis <denispolyarush1405@gmail.com>
2024-02-17 00:49:14 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+// 'use strict'
+// const target = 13; 
+// debugger
+// function way(target) {
+//     let current = 1;
+//     const multiple = function (current) {
+//         return current *= 3 `(${current} * 3)`;
+//     }
+//     const add = function (current) {
+//         return current += 5 `(${current} + 5)`
+//     }
+//     function findNumber(current, target) {
+//         if (current > target) {
+//             return null;
+//         }
+//         const result = multiple || add;
+//         return result || null
+//     }
+// }
+
+
+
