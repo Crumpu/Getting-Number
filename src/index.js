3853ff1ede05a0fa24f51e74109b71ab3a3c12bf
denis <denispolyarush1405@gmail.com>
2024-02-17 22:51:28 +0300

    Finihed

--- src/index.js
+++ src/index.js
@@ -1,22 +1,25 @@
-// 'use strict'
-// const target = 13; 
-// debugger
-// function way(target) {
-//     let current = 1;
-//     const multiple = function (current) {
-//         return current *= 3 `(${current} * 3)`;
-//     }
-//     const add = function (current) {
-//         return current += 5 `(${current} + 5)`
-//     }
-//     function findNumber(current, target) {
-//         if (current > target) {
-//             return null;
-//         }
-//         const result = multiple || add;
-//         return result || null
-//     }
-// }
+'use strict';
+let current = 1;
+const target = 13;
 
+function way(target) {
+  function findNumber(current, intermediateValue) {
+    if (current === target) {
+      return intermediateValue;
+    }
+    if (current > target) {
+      return null;
+    }
 
+    const multiple = findNumber(current * 3, `(${intermediateValue} * 3)`);
 
+    const add = findNumber(current + 5, `(${intermediateValue} + 5)`);
+
+    return multiple || add;
+  }
+  const result = findNumber(1, '1');
+
+  return result ? result : null;
+}
+
+console.log(way(target));
